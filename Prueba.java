d33f3ee79ee23d4b4e378113622761895095e148
diego <diegodiezprada@gmail.com>
2024-09-30 12:51:50 +0200

    añadida mi comida favorita

--- Prueba.java
+++ Prueba.java
@@ -4,5 +4,6 @@ public class Prueba{
 
 		System.out.println("Diez Prada");
 		System.out.println("tengo 19 años y nací en el año 2005");
+		System.out.println("mi comida favorita son las letejas de mi abuela");
 	}
 }
