38ff69819f1ae152f384bc799eed43a7f9d13407
diego <diegodiezprada@gmail.com>
2024-10-01 10:23:16 +0200

    quitados los apellidos y añadido grupo musical favorito

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,8 @@ public class Prueba{
 
 	public static void main(String args[]){
 
-		System.out.println("Diez Prada");
-		System.out.println("tengo 19 años y nací en el año 2005");
-		System.out.println("mi comida favorita son las letejas de mi abuela");
+		System.out.println("Tengo 19 años y nací en el año 2005");
+		System.out.println("Mi comida favorita son las letejas de mi abuela");
+		System.out.println("Mi grupo de música favorito es Ska-p");
 	}
 }
